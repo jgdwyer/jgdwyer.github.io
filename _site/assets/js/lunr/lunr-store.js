cd5328f0045be1251c8e9698e0804eb2643ad46a
John Dwyer <jgdwyer@gmail.com>
2024-02-07 20:37:23 -0800

    first post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,1 +1,7 @@
-var store = []
+var store = [{
+        "title": "What's the deal with pandas 3.0?",
+        "excerpt":"Pandas is a core tool for data practitioners. However, when pandas 3.0 is released (currently planned for April 2024), many data science workflows will break and need to be updated. Change is hard, but read on to see that this is ultimately a good thing for pandas users. First, let’s...","categories": ["blog"],
+        "tags": ["pandas","data science"],
+        "url": "/blog/pandas-3.0/",
+        "teaser": null
+      }]
